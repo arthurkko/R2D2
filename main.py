2195a6769c835dc5f2adc61f5ec14f44732a3929
arthurkko <arthurkko@gmail.com>
2021-09-28 05:21:17 -0300

    modified main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def conversational_api(app_id):
     question = "question"
     tries = 0
 
-    while (question!=""):
+    while (question!="sair"):
         try:
             with sr.Microphone() as source:
                 print("Pergunte!\n")
@@ -45,67 +45,82 @@ def conversational_api(app_id):
                 audio = r.listen(source)
 
             question = r.recognize_google(audio, language='pt-br')
-            print(
-                f"phrase: "
-                f"{question}\n\n")
+            print(f"phrase: {question}\n\n")
                 
         except Exception as e:
             print("Exception: " + str(e))
 
-        translation = language_translator.translate(
-            text=question,
-            model_id='pt-en').get_result()
-        print("translation:")
-        print(json.dumps(translation, indent=2, ensure_ascii=False))
-        print("\n\n")
-
-        question = translation['translations'][0]['translation'].replace(" ", "+")       # formatar a pergunta no formato da url
-
-        if tries==0:
-            url = "http://api.wolframalpha.com/v1/conversation.jsp?appid="+app_id+"&i="+question
-            response = json.loads(requests.get(url).text)
-            if "error" in response:
-                error = response["error"]
-            else:
-                print(f"{response['result']}\n")
-                tries += 1
-
-        else:                                       # a continuação da conversa ocorre em outra url
-            if "Wolfram|Alpha" not in error:
-                conv_id = response["conversationID"]
-                host = response["host"]
-                if "s" in response:                 # raros casos que response possui a key 's'
-                    s = response["s"]
-                    url = "http://"+host+"/api/v1/conversation.jsp?appid="+app_id+"&conversationid="+conv_id+"&i="+question+"&s="+s
-                else:
-                    url = "http://"+host+"/api/v1/conversation.jsp?appid="+app_id+"&conversationid="+conv_id+"&i="+question
-
-            response = json.loads(requests.get(url).text)
-            if "error" in response:
-                error = response["error"]
-            else:
-                print(f"{response['result']}\n")
-                error = ""
-
-        if error!="":
-            if ("No result is available" in error) and question!="": # 
-                print("\nNão sei responder a pergunta :/")
-        else:
+        if question!="sair":
             translation = language_translator.translate(
-                text=response['result'],
-                target='pt').get_result()
+                text=question,
+                model_id='pt-en').get_result()
+            print("translation:")
             print(json.dumps(translation, indent=2, ensure_ascii=False))
-            with open('text.wav', 'wb') as audio_file:     # criar arquivo .wav do text
-                try:
-                    audio_file.write(
-                        text_to_speech.synthesize(
-                            text=translation['translations'][0]['translation'],
-                            voice="pt-BR_IsabelaV3Voice",
-                            accept='audio/wav'        
-                        ).get_result().content)
-                except Exception as e:
-                    print("Exception: " + str(e))
-            playsound("text.wav")
+            print("\n\n")
+
+            question = translation['translations'][0]['translation'].replace(" ", "+")       # formatar a pergunta no formato da url
+
+            if tries==0:
+                url = "http://api.wolframalpha.com/v1/conversation.jsp?appid="+app_id+"&i="+question
+                response = json.loads(requests.get(url).text)
+                if "error" in response:
+                    error = response["error"]
+                else:
+                    print(f"{response['result']}\n")
+                    tries += 1
+
+            else:                                       # a continuação da conversa ocorre em outra url
+                if "Wolfram|Alpha" not in error:
+                    conv_id = response["conversationID"]
+                    host = response["host"]
+                    if "s" in response:                 # raros casos que response possui a key 's'
+                        s = response["s"]
+                        url = "http://"+host+"/api/v1/conversation.jsp?appid="+app_id+"&conversationid="+conv_id+"&i="+question+"&s="+s
+                    else:
+                        url = "http://"+host+"/api/v1/conversation.jsp?appid="+app_id+"&conversationid="+conv_id+"&i="+question
+
+                response = json.loads(requests.get(url).text)
+                if "error" in response:
+                    error = response["error"]
+                else:
+                    print(f"{response['result']}\n")
+                    translation = language_translator.translate(
+                        text=response['result'],
+                        target='pt').get_result()
+                    print(json.dumps(translation, indent=2, ensure_ascii=False))
+                    with open('text.wav', 'wb') as audio_file:     # criar arquivo .wav do text
+                        try:
+                            audio_file.write(
+                                text_to_speech.synthesize(
+                                    text=translation['translations'][0]['translation'],
+                                    voice="pt-BR_IsabelaV3Voice",
+                                    accept='audio/wav'        
+                                ).get_result().content)
+                        except Exception as e:
+                            print("Exception: " + str(e))
+                    playsound("text.wav")
+                    error = ""
+
+            if error!="":
+                if ("No result is available" in error) and question!="": # 
+                    print("\nNão sei responder a pergunta :/")
+            else:
+                print(f"{response['result']}\n")
+                translation = language_translator.translate(
+                    text=response['result'],
+                    target='pt').get_result()
+                print(json.dumps(translation, indent=2, ensure_ascii=False))
+                with open('text.wav', 'wb') as audio_file:     # criar arquivo .wav do text
+                    try:
+                        audio_file.write(
+                            text_to_speech.synthesize(
+                                text=translation['translations'][0]['translation'],
+                                voice="pt-BR_IsabelaV3Voice",
+                                accept='audio/wav'        
+                            ).get_result().content)
+                    except Exception as e:
+                        print("Exception: " + str(e))
+                playsound("text.wav")
 
     return error
 
